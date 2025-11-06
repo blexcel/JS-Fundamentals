0a92a96bea465a038f2c20539d1d6cef67f7b975
blexcel <obi.chioma123@yahoo.com>
2025-11-07 00:31:07 +0100

    Multiple Languages

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+let cLanguage = "C is fun";
+let pyLanguage = "Python is cool";
+let jsLanguage = "JavaScript is amazing";
+
+console.log(cLanguage);
+console.log(pyLanguage);
+console.log(jsLanguage);
