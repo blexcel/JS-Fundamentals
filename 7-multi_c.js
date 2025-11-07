7ef39298fa41bb7ceb31370f904f25802413ce03
blexcel <obi.chioma123@yahoo.com>
2025-11-07 15:53:09 +0100

    Looping through text

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,12 @@
 let userLang = process.argv[2];
 
-let args = Number(userLang);
+let x = Number(userLang);
 
-if (isNaN(args)) {
+if (isNaN(x)) {
     console.log("Missing number of occurrences");
 }
 else {
-    for (let i = 0; i < args; i++) {
+    for (let i = 0; i < x; i++) {
         console.log("C is fun");
     }
 }
